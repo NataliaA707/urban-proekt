e5e4340c20516798d4e0bbf25294136f57bacaaa
NataliaA707 <anisimovanatalia@icloud.com>
2024-08-25 21:47:45 +0300

    Add files via upload

---
+++ test2.py
@@ -0,0 +1,6 @@
+number_of_tasks=12
+number_of_hours=1.5
+course_name="Python"
+time=number_of_hours/number_of_tasks
+print("Курс"":" +str(course_name)+ "," "всего задач"":" +str(number_of_tasks)+ "," "затрачено часов"":"
++str(number_of_hours)+ "," "среднее время выполнения",time,"часа")
