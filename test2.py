65e3bc1b6e7158672ed41aacb164ead8b3e13490
NataliaA707 <anisimovanatalia@icloud.com>
2024-08-25 22:41:49 +0300

    Update test2.py

--- test2.py
+++ test2.py
@@ -1,6 +1,7 @@
-number_of_tasks=12
-number_of_hours=1.5
-course_name="Python"
-time=number_of_hours/number_of_tasks
-print("Курс"":" +str(course_name)+ "," "всего задач"":" +str(number_of_tasks)+ "," "затрачено часов"":"
-+str(number_of_hours)+ "," "среднее время выполнения",time,"часа")
+number_of_tasks = 12
+number_of_hours = 1.5
+course_name = 'Python'
+time = number_of_hours / number_of_tasks
+print('Курс'':' + str(course_name) + ',''всего задач'':' + str(number_of_tasks) +
+      ',''затрачено часов'':' + str(number_of_hours) + ',''среднее '
+                                                       'время выполнения', time, 'часа')
