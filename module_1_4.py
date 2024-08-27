ba3e3c696ae83677146f1dfc1a2b06bbf8192630
NataliaA707 <anisimovanatalia@icloud.com>
2024-08-28 01:00:03 +0300

    Update module_1_4.py

--- module_1_4.py
+++ module_1_4.py
@@ -1,9 +1,5 @@
 my_string = input('Введите Ваше ФИО: ')
-print(my_string)
-if my_string.count('а'):
-    print('Количество символов: ', my_string.count('а'))
-else:
-    print('Символов не найдено')
+print(len(my_string))
 print(my_string.upper())
 print(my_string.lower())
 print(my_string.replace(' ', ''))
